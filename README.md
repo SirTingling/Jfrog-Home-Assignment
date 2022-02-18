f594c817123961a0c932e594018ccb0f28a783fb
alexsherba29 <alexsherba29@gmail.com>
2022-02-18 14:43:25 +0200

    finish alex artifactory

--- README.md
+++ README.md
@@ -1,17 +1,35 @@
-# Jfrog-Home-Assignment
-JFrog Production Engineering Academy - Home Assignment
+# Jfrog Alex Home Assignment
 ## Description
+JFrog Production Engineering Academy - Alex Home Assignment
 
 ## Installation
+pip3 install -r requirements.txt
 
 ## Usage
+python3 alex_artifactory.py [-h] [-p] [-v] [-gsi] [-lr] [-cr] [-dr] [-ur] [-cu] [-du]
+
+optional arguments: 
+
+  -h, --help            show this help message and exit
+  -p, --system_ping     Return ping from the system
+  -v, --system_version  Return system version
+  -gsi, --get_storage_info
+                        Get storage info
+  -lr, --list_repositories
+                        List repositories
+  -cr, --create_repository
+                        Create repository
+  -dr, --delete_repository
+                        Delete repository
+  -ur, --update_repository
+                        Update repository
+  -cu, --create_user    Create user
+  -du, --delete_user    Delete user
 
 ## Resources
 https://academy.jfrog.com/jfrog-artifactory-overview-2020/443201
-https://github.com/jfrog/artifactory-scripts/blob/master/REST-API-Examples/python-get.py
-https://github.com/jfrog/artifactory-scripts/blob/master/REST-API-Examples/python-post.py
 https://github.com/jfrog/artifactory-scripts/tree/master/
+https://github.com/jfrog/artifactory-scripts/blob/master/REST-API-Examples
 https://www.jfrog.com/confluence/display/RTF
 https://www.jfrog.com/confluence/display/JFROG/Artifactory+REST+APIREST-API-Examples
-https://www.jfrog.com/confluence/display/JFROG/Artifactory+REST+API
 https://docs.python.org/3/library/argparse.html
--- alex_artifactory.py
+++ alex_artifactory.py
@@ -1,77 +1,164 @@
-"""
-CLI in python to manage an Artifactory SaaS instance via its API.
-"""
-
 import argparse
+from traceback import print_tb
 import requests
 import json
+import sys
 
-
-parser = argparse.ArgumentParser(description='JFrog Production Engineering Academy - Alex Scherbakov Home Assignments.')
-parser.add_argument("-p", "--system_ping", action="store_true",help="Return ping from the system") # store_true : storing the values True or False
-parser.add_argument("-v", "--system_version", action="store_true",help="Return system version")
-parser.add_argument("-cu", "--create_user", action="store_true",help="Create user") 
-parser.add_argument("-du", "--delete_user", action="store_true",help="Delete user") 
-parser.add_argument("-gsi", "--get_storage_info", action="store_true",help="Get storage info")
-args = parser.parse_args()
-
-
-
-def main():
-    flag_dic = {"system_ping": False, "system_version": False}
-    if args.system_ping:
-        flag_dic["system_ping"] = True
-        respod_from_the_server("api/system/ping", flag_dic)
-    elif args.system_version:
-        flag_dic["system_version"] = True
-        respod_from_the_server("api/system/version", flag_dic)
-    elif args.create_user:
-        create_user(input("Enter username: "), input("Enter password: "), input("Enter email: "))
-    elif args.delete_user:
-        respod_from_the_server("api/security/users/delete")
-    elif args.get_storage_info:
-        respod_from_the_server("api/storage/info")
-    else:
-        print("Please enter a valid argument")
-
-def respod_from_the_server(api="", flag_dic={}):
+def parser_args():
     """
-    This function is used to get the response from the server.
+    This function is used to parse the arguments.
     """
-    username = "alexscher"
-    password = "Alex0246802"
-    artifactory = "https://alexscher.jfrog.io/artifactory/" #artifactory URL
+    parser = argparse.ArgumentParser(description='JFrog Production Engineering Academy - Alex Scherbakov Home Assignments.')
+    parser.add_argument("-p",   "--system_ping",       action="store_true", help="Return ping from the system") # store_true : storing the values True or False
+    parser.add_argument("-v",   "--system_version",    action="store_true", help="Return system version")
+    parser.add_argument("-si",  "--storage_info",      action="store_true", help="Get storage info")
+    parser.add_argument("-lr",  "--list_repositories", action="store_true", help="List repositories") 
+    parser.add_argument("-cr",  "--create_repository", action="store_true", help="Create repository")
+    parser.add_argument("-ur",  "--update_repository", action="store_true", help="Update repository")
+    parser.add_argument("-cu",  "--create_user",       action="store_true", help="Create user")
+    parser.add_argument("-du",  "--delete_user",       action="store_true", help="Delete user") 
+    args = parser.parse_args()
+    return args
+
+class Artifactory:
+    def __init__(self):
+        self.username, self.password, self.artifactory_url, self.headers = self.user_authentication()
+        self.args = parser_args()
+        
+    def user_authentication(self):
+        """
+        Get username and password from the user.
+        """
+        username = "alexscherba" #input("Input username: ")  
+        password = "Alex0246802" #input("Input password: ")
+        artifactory_url = f"https://{username}.jfrog.io/artifactory/"
+        token_url = "api/security/token"
+
+        r = requests.get(artifactory_url + token_url, auth = (username, password))
+        if r.status_code == 200: # if the response is 200, then the token is valid
+            headers = {'X-JFrog-Api': r.headers.get('X-JFrog-Api'), 'Content-Type': 'application/json'}
+            return username, password, artifactory_url, headers
+        else:
+            sys.exit("Cant get token information\n" % r.content.decode('utf-8'))
+
+    def pass_args(self):
+        """
+        Pass the arguments to the functions.
+        """
+        if self.args.system_ping:
+            self.system_ping()
+        elif self.args.system_version:
+            self.system_version()
+        elif self.args.storage_info:
+            self.storage_info()
+        elif self.args.list_repositories:
+            self.list_repositories()
+        elif self.args.create_user:
+            self.create_user()
+        elif self.args.delete_user:
+            self.delete_user()
+        elif self.args.create_repository:
+            self.create_repository()
+        elif self.args.delete_repository:
+            self.delete_repository()
+        elif self.args.update_repository:
+            self.update_repository()
+        else:
+            print("Please enter a valid argument")
+
+    def system_ping(self):
+        """
+        Ping the server
+        """
+        r = requests.get(self.artifactory_url + "api/system/ping", auth = (self.username, self.password))
+        if r.status_code == 200:
+            print("Ping status: " + r.content.decode('utf-8'))
+        else:
+            print("Ping failed")
     
-    url = artifactory + api
-    r = requests.get(url, auth = (username, password)) #this script is only for API methods that use GET
+    def system_version(self):
+        """
+        Get the version of the server
+        """
+        r = requests.get(self.artifactory_url + "api/system/version", auth = (self.username, self.password))
+        if r.status_code == 200:
+            print("Version: " + r.content.decode('utf-8'))
+        else:
+            print("Version failed")
+    
+    def storage_info(self):
+        """
+        Get the storage info
+        """
+        r = requests.get(self.artifactory_url + "api/storageinfo", auth = (self.username, self.password))
+        if r.status_code == 200:
+            print(json.dumps(r.json(), indent = 4))
+        else:
+            print("Storage info failed")
+    
+    def list_repositories(self):
+        """
+        List all repositories
+        """
+        r = requests.get(self.artifactory_url + "api/repositories", auth = (self.username, self.password))
+        if r.status_code == 200:
+            print(json.dumps(r.json(), indent = 4))
+        else:
+            print("List repositories failed")
 
-    if r.status_code == 200 and flag_dic["system_ping"]:
-        print("Ping status: " + r.content.decode('utf-8'))
-    elif r.status_code == 200 and flag_dic["system_version"]:
-        print(r.headers.get('X-JFrog-Version'))
-    else:
-        print("Fail")
-        response = json.loads(r.content)
-        print(response["errors"])
+    def create_user(self):
+        """
+        Create a user in Artifactory
+        """
+        create_user_dict = {}
+        create_user_dict["username"] = input("Input username: ")
+        create_user_dict["password"] = input("Input password: ")
+        create_user_dict["admin"] = False
+        create_user_dict["email"] = input("Input email: ")
+        r = requests.put(self.artifactory_url + "api/security/users/" + create_user_dict["username"], headers=self.headers, data=json.dumps(create_user_dict).encode('utf-8'), auth = (self.username, self.password))
+        if r.status_code == 201:
+            print("User created")
+        else:
+            print("User creation failed \n" + r.content.decode('utf-8'))
 
-        print("x-request-id : " + r.headers['x-request-id'])
-        print("Status Code : " + r.status_code)
-
-def create_user(username, password, email="", admin=False):
-    """
-    Create a user in Artifactory.
-    """
-    url = f"https://alexscher.jfrog.io/artifactory/api/security/users/{username}"
-    data = {
-        "name": username,
-        "password": password,
-        "admin": admin,
-        "email": email
-    }
-    r = requests.put(url, data=data)
-    print(r.content)
-
-
-
+    def delete_user(self):
+        """
+        Delete a user in Artifactory
+        """
+        user_name = input("Input user name: ")
+        r = requests.delete(self.artifactory_url + "api/security/users/" + user_name, auth = (self.username, self.password))
+        if r.status_code == 200:
+            print("User deleted successfully")
+        else:
+            print("User deletion failed \n" + r.content.decode('utf-8'))
+    
+    def create_repository(self):
+        """
+        Create a repository in Artifactory
+        """
+        create_user_dict = {}
+        create_user_dict["key"] = input("Input repository name: ")
+        create_user_dict["rclass"] = "local"
+        create_user_dict["packageType"] = input("Package type: ")# "maven"
+        create_user_dict["description"] = input("Input repository description: ")
+        r = requests.put(self.artifactory_url + "api/repositories/" + create_user_dict["key"], headers=self.headers, data=json.dumps(create_user_dict).encode('utf-8'), auth = (self.username, self.password))
+        if r.status_code == 200:
+            print("Repository created successfully")
+        else:
+            print("Repository creation failed \n" + str(r.status_code) + " " + str(r.content.decode('utf-8')))
+    
+    def update_repository(self):
+        """
+        Update a repository in Artifactory
+        """
+        update_repository_dict = {}
+        update_repository_dict["description"] = input("Input repository description: ")
+        r = requests.post(self.artifactory_url + "api/repositories/" + update_repository_dict["key"], headers=self.headers, data=json.dumps(update_repository_dict).encode('utf-8'), auth = (self.username, self.password))
+        if r.status_code == 200:
+            print("Repository updated successfully")
+        else:
+            print("Repository update failed\n" + str(r.status_code) + " " + str(r.content.decode('utf-8')))
+    
 if __name__ == "__main__":
-    main()
+    artifactory = Artifactory()
+    artifactory.pass_args()
